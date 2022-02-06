e8dcc68e7ca3f1e29ca6769911a422062d6d2a52
Aleksei Nechaev <anechaev@devexperts.com>
2022-02-06 23:19:36 +0300

    init proj

---
+++ src/main/java/ru/nechay/battlecode/BattlecodeApplication.java
@@ -0,0 +1,13 @@
+package ru.nechay.battlecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlecodeApplication.class, args);
+	}
+
+}
